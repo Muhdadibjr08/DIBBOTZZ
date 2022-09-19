9c6ec15d802769728e5a7b1fbae103a0f22d4049
Muhdadibjr08 <113897758+Muhdadibjr08@users.noreply.github.com>
2022-09-19 21:06:10 +0800

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `JAN SOK ASIK KALO DELAY NJG`
+let info = `YUK GAY ABE BROWN🤧`
 
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 /*const message = {
